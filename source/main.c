b7328f7be5d80b26c68bbc9e1f75244028f9105f
trainraider <trainraider7@gmail.com>
2026-02-01 23:06:23 -0500

    file opening

--- source/main.c
+++ source/main.c
@@ -11,6 +11,24 @@
 AdwApplication* app          = NULL;
 GIcon*          new_tab_icon = NULL;
 
+void
+on_open(GtkApplication* self, GFile** files, gint n_files, gchar* hint, gpointer user_data)
+{
+        (void)user_data;
+        (void)hint;
+        AsceticAppWindow* window  = ascetic_app_window_new();
+        for (gint i = 0; i < n_files; i++) {
+                GFile*            file    = files[i];
+                AdwTabPage*       tab     = new_tab(GTK_WIDGET(window), window);
+                WebKitWebView*    webview = tab_get_webview(tab);
+                gchar*            uri     = g_file_get_uri(file);
+                webkit_web_view_load_uri(webview, uri);
+                g_free(uri);
+        }
+        gtk_window_set_application(GTK_WINDOW(window), GTK_APPLICATION(self));
+        gtk_window_present(GTK_WINDOW(window));
+}
+
 void activate(GtkApplication* app, gpointer user_data)
 {
         (void)user_data;
@@ -33,8 +51,9 @@ int main(int argc, char* argv[])
         uri_init();
         browser_session_init();
         new_tab_icon = g_themed_icon_new("applications-internet-symbolic");
-        app          = adw_application_new(APP_ID, G_APPLICATION_DEFAULT_FLAGS);
+        app          = adw_application_new(APP_ID, G_APPLICATION_HANDLES_OPEN);
         g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+        g_signal_connect(app, "open", G_CALLBACK(on_open), NULL);
         int status = g_application_run(G_APPLICATION(app), argc, argv);
 
         g_object_unref(app);
